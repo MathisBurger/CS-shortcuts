f66e8aaf443060acb2d16c3d73f187ddc945e69b
Mathis Burger <handymathis@gmail.com>
2024-04-11 23:01:10 +0200

    [TASK] Added functionality

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -21,8 +21,6 @@ partial class Form1
         base.Dispose(disposing);
     }
 
-    #region Windows Form Designer generated code
-
     /// <summary>
     ///  Required method for Designer support - do not modify
     ///  the contents of this method with the code editor.
@@ -32,8 +30,42 @@ partial class Form1
         this.components = new System.ComponentModel.Container();
         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
         this.ClientSize = new System.Drawing.Size(800, 450);
-        this.Text = "Form1";
-    }
+        this.Text = "CS Chortcuts";
 
-    #endregion
+        Label lbl_1 = new Label();
+        lbl_1.Width = 300;
+        lbl_1.Text = "ALT + Q => M4A1-S / AK-47 + Kevlar";
+        this.Controls.Add(lbl_1);
+        
+        Label lbl_2 = new Label();
+        lbl_2.Width = 300;
+        lbl_2.Top = 20;
+        lbl_2.Text = "ALT + W => AWP + Kevlar";
+        this.Controls.Add(lbl_2);
+        
+        Label lbl_3 = new Label();
+        lbl_3.Width = 300;
+        lbl_3.Top = 40;
+        lbl_3.Text = "ALT + E => XM + Kevlar";
+        this.Controls.Add(lbl_3);
+        
+        Label lbl_4 = new Label();
+        lbl_4.Width = 300;
+        lbl_4.Top = 60;
+        lbl_4.Text = "ALT + R => Kevlar";
+        this.Controls.Add(lbl_4);
+        
+        Label lbl_5 = new Label();
+        lbl_5.Width = 300;
+        lbl_5.Top = 80;
+        lbl_5.Text = "ALT + T => Grenades";
+        this.Controls.Add(lbl_5);
+        
+        Label lbl_6 = new Label();
+        lbl_6.Width = 300;
+        lbl_6.Top = 100;
+        lbl_6.Text = "ALT + Z => TEC + Kevlar";
+        this.Controls.Add(lbl_6);
+    }
+    
 }
--- Form1.cs
+++ Form1.cs
@@ -1,9 +1,87 @@
+using System.Runtime.InteropServices;
+
 namespace CS_Shortcuts;
 
 public partial class Form1 : Form
 {
+    
+    [DllImport("user32.dll")]
+    private static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vk);
+    
+    
     public Form1()
     {
         InitializeComponent();
+        this.KeyPreview = true;
+        RegisterHotKey(this.Handle, 0, 1, Keys.Q.GetHashCode());
+        RegisterHotKey(this.Handle, 1, 1, Keys.W.GetHashCode());
+        RegisterHotKey(this.Handle, 2, 1, Keys.E.GetHashCode());
+        RegisterHotKey(this.Handle, 3, 1, Keys.R.GetHashCode());
+        RegisterHotKey(this.Handle, 4, 1, Keys.T.GetHashCode());
+        RegisterHotKey(this.Handle, 5, 1, Keys.Z.GetHashCode());
+    }
+
+    /// <summary>
+    /// Windows override to capture keybind
+    /// </summary>
+    /// <param name="m">The message from os</param>
+    protected override void WndProc(ref Message m)
+    {
+        base.WndProc(ref m);
+
+        if (m.Msg == 0x0312)
+        {
+            Keys key = (Keys)(((int)m.LParam >> 16) & 0xFFFF);
+            int modifier = ((int)m.LParam & 0xFFFF);
+            if (modifier == 1)
+            {
+                Console.WriteLine(key);
+                WriteBuyShortcut(key);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Writes a shortcut to keyboard by converting it
+    /// into the CSGO sequence 
+    /// </summary>
+    /// <param name="key">The key that is pressed for shortcut</param>
+    private void WriteBuyShortcut(Keys key)
+    {
+        switch (key)
+        {
+            case Keys.Q:
+                // Sends M4A1-S and Kevlar
+                this.SendAsThread("b1242b");
+                break;
+            case Keys.W:
+                // Sends AWP and Kevlar
+                this.SendAsThread("b1245b");
+                break;
+            case Keys.E:
+                // Send XM and Kevlar
+                this.SendAsThread("b1232b");
+                break;
+            case Keys.R:
+                // Sends kevlar
+                this.SendAsThread("b12b");
+                break;
+            case Keys.T:
+                // Sends grenades
+                this.SendAsThread("b1143b");
+                break;
+            case Keys.Z:
+                // Sends tec and kevlar
+                this.SendAsThread("b1224b");
+                break;
+        }
+    }
+
+    private void SendAsThread(string key)
+    {
+        new Thread(() =>
+        {
+            SendKeys.SendWait(key);
+        }).Start();
     }
 }
